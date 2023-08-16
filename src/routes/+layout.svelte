ade1269932ff4bd5a8da902a39ff959a2d45eb2f
Anders Lorentsen <anders.lorentsen@gmail.com>
2023-08-16 14:25:37 +0200

    better way to toggle debug console

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,13 @@
 <script>
     import { onMount, onDestroy } from "svelte";
+    import { fly } from "svelte/transition";
     import { debug_console, debug } from "$lib/debug_console.js";
     import LocaleSelector from "$components/LocaleSelector.svelte";
     import { t } from "svelte-intl-precompile";
     // TEMP debug
     import { delete_database } from "$lib/dictionary.js";
 
-    let small_console = false;
+    let console_hidden = false;
     // TEMP debug
     /*
     let x = 0;
@@ -26,11 +27,65 @@
     */
 
     let n = 0;
+
+    function hit_zone(x, y, w, h) {
+        if (y >= (3/4)*h) {
+            // lower quartant of screen
+            return x > w / 2 ? "right" : "left";
+        }
+        return null;
+    }
+    let hz_expected_next = "any";
+    let hz_timer = null;
+    let hz_count = 0;
+
+    function hz_clear() {
+        if (hz_timer) window.clearTimeout(hz_timer);
+        hz_timer = null;
+        hz_count = 0;
+        hz_expected_next = "any";
+    }
+
+    function c(ev) {
+        const { clientX: x, clientY: y } = ev;
+        const w = window.visualViewport.width;
+        const h = window.visualViewport.height;
+        const hz = hit_zone(x, y, w, h);
+        if (hz === null) {
+            console.log("no hit, reset");
+            hz_clear();
+            return;
+        }
+
+        if (hz_count === 0) {
+            console.log("first hit:", hz);
+            hz_expected_next = hz === "left" ? "right" : "left";
+            hz_count++;
+        } else {
+            if (hz !== hz_expected_next) {
+                console.log("not expected, reset");
+                hz_clear();
+                return;
+            }
+
+            if (++hz_count >= 4) {
+                console.log("HZ COMPLETE!");
+                console_hidden = false;
+            } else {
+                hz_expected_next = hz === "left" ? "right" : "left";
+                console.log(`hit #${hz_count}, next expected: ${hz_expected_next}`);
+            }
+        }
+        //console.log(`(y=${y}, x=${x}). h=${h}, w=${w}`);
+        //console.log(`hit_zone: ${hit_zone(x, y, w, h)}`);
+    }
 </script>
 
 <svelte:head>
     <title>Giellatekno Webdict</title>
 </svelte:head>
+
+<svelte:window on:click={c} />
 
 <!-- TEMP debug -->
 <span class="temp">
@@ -54,29 +109,26 @@
     </main>
 </div>
 
-<div
-    class="console"
-    class:small={small_console}
-    on:click={() => small_console = !small_console}
-    on:keyup={() => null}
->
-    <h3>dev console</h3>
-    <!--
-    <button on:click={ev => { debug("message: " + n++); ev.stopPropagation(); }}>Add message</button>
-    -->
-    {#if !small_console}
+{#if !console_hidden}
+    <div
+        class="console"
+        class:hidden={console_hidden}
+        on:click={() => console_hidden = true}
+        on:keyup={() => null}
+        transition:fly={{ y: 100 }}
+    >
+        <h3>dev console</h3>
+        <button on:click={ev => { debug("message: " + n++); ev.stopPropagation(); }}>Add message</button>
         <div class="inner" id="debug-console">
-            <!--
             {#each Array(100).fill(0).map(_0 => "log message: something somethng()") as msg}
                 <div>{msg}</div>
             {/each}
-            -->
             {#each $debug_console as msg}
                 <div>{msg}</div>
             {/each}
         </div>
-    {/if}
-</div>
+    </div>
+{/if}
 
 <style>
     div.console {
@@ -96,12 +148,15 @@
 
     div.inner {
         overflow-y: scroll;
+        scroll-padding: 0 0 -50px 0;
         padding: 12px;
         border: 1px solid black;
     }
 
-    div.console.small {
-        height: 10vh;
+    div.console.hidden {
+        height: 0vh;
+        padding: 0;
+        width: 0;
     }
 
     div.wrapper {
